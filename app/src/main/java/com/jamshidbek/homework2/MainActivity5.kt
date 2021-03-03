a832b003156e5a19e750a51036b056a4d99bef04
Jamshidbek <zamsidbekmamazonov@gmail.com>
2021-03-03 22:12:02 +0500

    Hope it will pass!

---
+++ app/src/main/java/com/jamshidbek/homework2/MainActivity5.kt
@@ -0,0 +1,11 @@
+package com.jamshidbek.homework2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main5)
+    }
+}
